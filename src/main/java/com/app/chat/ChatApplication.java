2aa701eea8ed42b8b32b9c27c7d077559814a4c7
Tran Duc Tung <tranductung07012004@gmail.com>
2026-05-18 16:51:05 +0700

    chore(backend): second commit

--- src/main/java/com/app/chat/ChatApplication.java
+++ src/main/java/com/app/chat/ChatApplication.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatApplication {
 
+    volatile boolean isRunning = true;
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+
+		String a = "Hello, World!";
+		a.wait(); // This will cause a compile-time error because wait() is not defined for String
+		a.notify(); // This will also cause a compile-time error for the same reason
+		a.notifyAll(); // This will also cause a compile-time error for the same reason
+		// to add 1 commit
+		String b = "Hello, World!";
 	}
 
 }
